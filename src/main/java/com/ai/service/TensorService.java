23415bb820eb27e7cc0bd9e2c93ecb5eee4ecf91
xuliqian <xuliqian@cmos.chinamobile.com>
2017-11-30 21:23:58 +0800

    0

---
+++ src/main/java/com/ai/service/TensorService.java
@@ -0,0 +1,13 @@
+package com.ai.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by nowcoder on 2016/6/26.
+ */
+@Service
+public class TensorService {
+    public String say() {
+        return "This is from ToutiaoService";
+    }
+}
